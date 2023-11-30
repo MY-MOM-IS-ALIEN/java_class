879d72a08d198a0c16f5d46cab9bea723384b619
MY-MON-IS-ALIEN <alsdn5471@naver.com>
2023-11-30 16:20:37 +0900

    산술연산자

---
+++ src/ch01_variables/Ex01_Variables.java
@@ -0,0 +1,19 @@
+package ch01_variables;
+
+public class Ex01_Variables {
+
+	public static void main(String[] args) {
+		// 본인의 이름, 나이, 키, 주소, 전화번호를 입력할 수 있는 변수를 각각 선언해봅시다
+		String name = "안민우";
+		int age = 28;
+		double height = 177.7;
+		String address = "인천 부평구";
+		String mobile = "010-7152-6196";
+	    String insta_address = "m1nw_oooo";
+	    
+	    System.out.println("제 이름은 " + name + " 입니다." );
+	    
+	    
+	}
+
+}
---
+++ src/ch02_operator/Ex02_Operator.java
@@ -0,0 +1,30 @@
+package ch02_operator;
+
+public class Ex02_Operator {
+
+	public static void main(String[] args) {
+     //산술연산자
+	// num1,num2 두개의 정수형 변수를 선언하고 값은 마음대로
+	// 두 변수의 +,-,*,/,% 연산결과를 print()를 활용하여 출력
+	
+	 int num1 = 15;
+	 int num2 = 13;
+	 
+	 System.out.println(num1 + num2);
+	 System.out.println(num1 - num2);
+	 System.out.println(num1 * num2);
+	 System.out.println(num1 / num2);
+	 System.out.println(num1 % num2);
+	 
+	 System.out.println(num1 < num2);
+	 System.out.println(num1 > num2);
+	 System.out.println(num1 <= num2);
+	 System.out.println(num1 >= num2);
+	 System.out.println(num1 == num2);
+	 System.out.println(num1 != num2);
+	 
+	 
+
+	}
+
+}
