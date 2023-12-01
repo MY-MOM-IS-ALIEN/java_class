0812c2bfa7ee00e5b8818d348af501a5b4893005
MY-MON-IS-ALIEN <alsdn5471@naver.com>
2023-12-01 13:15:01 +0900

    스ㅡ캐너 넥스트라인

--- src/ch02_operator/Ex05_IncreaseDecreaseOperator.java
+++ src/ch02_operator/Ex05_IncreaseDecreaseOperator.java
@@ -6,6 +6,7 @@ public class Ex05_IncreaseDecreaseOperator {
 		// TODO Auto-generated method stub
         // 증감연산자
 		int num1 = 10 , num2 = 0, num3 = 0;
+		
 		System.out.println("num1: " + num1 + " num2: " + num2 + " num3: " + num3 );
 		num2 = ++num1;
 		System.out.println("num1: " + num1 + " num2: " + num2 + " num3: " + num3 );
--- src/ch03_scanner/Ex01_Scanner.java
+++ src/ch03_scanner/Ex01_Scanner.java
@@ -8,10 +8,14 @@ public class Ex01_Scanner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      // scanner 라는 이름의 Scanner 클래스 객체 선언
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("숫자를 입력하세요: ");		
-		int num1 = scanner.nextInt();
-		System.out.println(num1);
+        Scanner scanner = new Scanner(System.in);
+//		System.out.print("숫자를 입력하세요: ");		
+//		int num1 = scanner.nextInt();
+//		System.out.println(num1);
+		
+		System.out.print("주소를 입력하세요: ");
+		String address = scanner.nextLine();
+		System.out.println(address);
 		
 		
 	}
