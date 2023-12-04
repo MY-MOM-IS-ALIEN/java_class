de21752d03752aa7e3d89c2ee6c42af47b5cd5fc
MY-MON-IS-ALIEN <alsdn5471@naver.com>
2023-12-04 10:42:57 +0900

    if문복습2

---
+++ src/ch04_if/Ex07_IfExample.java
@@ -0,0 +1,37 @@
+package ch04_if;
+
+import java.util.Scanner;
+
+public class Ex07_IfExample {
+
+	public static void main(String[] args) {
+		// 성적 출력 예제 응용
+		// 입력값 : 학년 , 점수
+		// 처리
+		// 1~3학년은 60점 이상이면 합격
+		// 4학년은 70점이상이여야 합격
+		
+		  Scanner scanner = new Scanner(System.in);
+	      System.out.println("학년을 입력하세요");
+	      Scanner sc = new Scanner(System.in);
+	      System.out.println("점수를 입력하세요");
+	      int num1 = scanner.nextInt();
+	      int num2 = scanner.nextInt();
+	      
+	      if(num1 == 4) {
+	    	  if(num2 >= 70) {
+	    		  System.out.println("합격");
+	    	  }else {
+	    		  System.out.println("불합격");
+	    	  }
+	      }else if (num1 == 1 || num1 == 2 || num1 == 3) {
+	    		if (num2 >= 60) {
+	    			System.out.println("합격");
+	    		} else {
+	    			System.out.println("불합격");
+	    		}
+	      }
+	      
+      
+  	}
+ }
