d7eb8ea827b6957b3c61706f76b58ef365d26f6d
MY-MON-IS-ALIEN <alsdn5471@naver.com>
2023-12-05 13:27:19 +0900

    중첩 for문, 구구단 예제

--- src/ch06_for/Ex07_NestedFor.java
+++ src/ch06_for/Ex07_NestedFor.java
@@ -17,5 +17,6 @@ public class Ex07_NestedFor {
 
 
 
+
     }
 }
---
+++ src/ch06_for/Ex08_forEx.java
@@ -0,0 +1,19 @@
+package ch06_for;
+
+public class Ex08_forEx {
+    public static void main(String[] args) {
+
+        for(int i = 2; i <= 9; i++){// 단 수
+            System.out.println(i+"단");
+            for(int j = 1; j <= 9; j++) {// 곱하는 수
+                System.out.print(i + "x" + j + "=" + (i * j) + "  ");
+                if (j == 9) {
+                    System.out.println();
+                }
+
+            }
+        }
+
+
+    }
+}
