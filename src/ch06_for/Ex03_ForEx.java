ce84a60d7c62b31cebf09b77ce8ee4a02deab89e
MY-MON-IS-ALIEN <alsdn5471@naver.com>
2023-12-05 09:51:51 +0900

    For문 예제

--- src/ch06_for/Ex03_ForEx.java
+++ src/ch06_for/Ex03_ForEx.java
@@ -3,13 +3,20 @@ package ch06_for;
 public class Ex03_ForEx {
     public static void main(String[] args) {
         //1부터 10까지 합계를 출력
+        //1부터 더해지는 과정까지 출력하기
 
         int sum = 0;
 
-        for(int i = 0; i <= 10; i++){
+        for(int i = 1; i <= 10; i++){
+            if(i < 10){
+                System.out.print(i + "+");
+            }else{
+                System.out.print(i + "=");
+            }
             sum = sum + i;
+
         }
-        System.out.println(sum);
+        System.out.print(sum);
 
 
 
